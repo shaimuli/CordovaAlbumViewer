30de2ae0afd1a89fba863928fcab6d06a8f07f68
shaimuli <shaimuli@gmail.com>
2015-06-02 23:16:03 +0300

    asd
    asd

--- scripts/index.js
+++ scripts/index.js
@@ -10,18 +10,25 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        document.addEventListener("backbutton", function (e) {
-            if ($.mobile.activePage.is('#homepage')) {
-                e.preventDefault();
-                navigator.app.exitApp();
-            }
-            else {
-                navigator.app.backHistory()
-            }
-        }, false);
+        document.addEventListener('resume', onResume.bind(this), false);
+        document.addEventListener("backbutton", onBackKeyDown, false);
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
+
+    function onBackKeyDown(e) {
+        alert("asdasd");
+        e.preventDefault();
+        navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No");
+        // Prompt the user with the choice
+    }
+
+    function onConfirm(button) {
+        if (button == 2) {//If User selected No, then we just do nothing
+            return;
+        } else {
+            navigator.app.exitApp();// Otherwise we quit the app.
+        }
+    }
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here.
