69c333ede798c805c1e68a39bd0c31e87651ffb6
shaimuli <shaimuli@gmail.com>
2015-06-03 00:03:05 +0300

    asd

--- scripts/index.js
+++ scripts/index.js
@@ -5,32 +5,49 @@
 (function () {
     "use strict";
 
-    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );    
+    //document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );    
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener('resume', onResume.bind(this), false);
-        document.addEventListener("backbutton", onBackKeyDown, false);
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
+    //function onDeviceReady() {
+    //    // Handle the Cordova pause and resume events
+    //    document.addEventListener( 'pause', onPause.bind( this ), false );
+    //    document.addEventListener('resume', onResume.bind(this), false);
+    //    document.addEventListener("backbutton", onBackKeyDown, false);
+    //    // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+    //};
 
-    function onBackKeyDown(e) {
-        if ($('.ui-page-active').attr('id') == 'main') {
-            alert("111");
-            exitAppPopup();
-        } else {
-            alert("2222");
-            exitAppPopup();
-        }
-        //e.preventDefault();
-        //navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No");
-        //alert("asdasd");
-        // Prompt the user with the choice
-    }
+    //function onBackKeyDown(e) {
+    //    if ($('.ui-page-active').attr('id') == 'main') {
+    //        alert("111");
+    //        exitAppPopup();
+    //    } else {
+    //        alert("2222");
+    //        exitAppPopup();
+    //    }
+    //    //e.preventDefault();
+    //    //navigator.notification.confirm("Are you sure you want to exit ?", onConfirm, "Confirmation", "Yes,No");
+    //    //alert("asdasd");
+    //    // Prompt the user with the choice
+    //}
+
+    document.addEventListener('deviceready', function() {
+        var exitApp = false, intval = setInterval(function (){exitApp = false;}, 1000);
+        document.addEventListener("backbutton", function (e){
+            e.preventDefault();
+            if (exitApp) {
+                clearInterval(intval) 
+                (navigator.app && navigator.app.exitApp()) || (device && device.exitApp())
+            }
+            else {
+                clearInterval(intval) 
+                (navigator.app && navigator.app.exitApp()) || (device && device.exitApp())
+                //exitApp = true
+                //history.back(1);
+            } 
+        }, false);
+    }, false
 
     function exitAppPopup() {
-        alert("333");
+        //alert("333");
          //navigator.app.exitApp();
         navigator.notification.confirm('Sure you want to exit!',onConfirm, 'Confirm', ['Cancel','Exit']); 
     }
